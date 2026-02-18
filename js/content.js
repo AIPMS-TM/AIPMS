9dd309fbfe9c1f5d0c0536ab6f75960d128614db
OwnerAIPMS <chdocumenti2022@gmail.com>
2026-02-18 15:25:42 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,14 +1,14 @@
 const content = {
   it: {
-    history: "Fondata nel 2018, AIPMS nasce per unire innovazione e progresso, promuovendo una visione futura in cui l’Intelligenza Artificiale opera come alleata dell’umanità, a servizio dello sviluppo, della conoscenza e del progresso collettivo.",
+    history: "Fondata nel 2016, AIPMS nasce per unire innovazione e progresso, promuovendo una visione futura in cui l’Intelligenza Artificiale opera come alleata dell’umanità, a servizio dello sviluppo, della conoscenza e del progresso collettivo.",
     message: "Il nostro sito è in costruzione. Torna presto a scoprirlo!"
   },
   en: {
-    history: "Founded in 2018, AIPMS was created to unite innovation and progress, promoting a future vision in which Artificial Intelligence acts as an ally of humanity, serving development, knowledge, and collective progress.",
+    history: "Founded in 2016, AIPMS was created to unite innovation and progress, promoting a future vision in which Artificial Intelligence acts as an ally of humanity, serving development, knowledge, and collective progress.",
     message: "Our website is under construction. Check back soon!"
   },
   de: {
-    history: "Gegründet 2018, wurde AIPMS geschaffen, um Innovation und Fortschritt zu vereinen, mit einer Vision für die Zukunft, in der Künstliche Intelligenz als Verbündete der Menschheit dient, zur Förderung von Entwicklung, Wissen und kollektiven Fortschritt.",
+    history: "Gegründet 2016, wurde AIPMS geschaffen, um Innovation und Fortschritt zu vereinen, mit einer Vision für die Zukunft, in der Künstliche Intelligenz als Verbündete der Menschheit dient, zur Förderung von Entwicklung, Wissen und kollektiven Fortschritt.",
     message: "Unsere Website befindet sich im Aufbau. Schauen Sie bald wieder vorbei!"
   }
 };
