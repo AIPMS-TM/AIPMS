2b618c8d9276e4259b1e6bb3f87777b45be31a59
OwnerAIPMS <chdocumenti2022@gmail.com>
2026-02-18 16:22:40 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -5,17 +5,17 @@ const content = {
   it: {
     history: "Fondata nel 2016, AIPMS nasce per unire innovazione e progresso, promuovendo una visione futura in cui l’Intelligenza Artificiale opera come alleata dell’umanità, a servizio dello sviluppo, della conoscenza e del progresso collettivo.",
     message: "Il nostro sito è in costruzione. Torna presto a scoprirlo!",
-    labLink: `<a href="lab.html">Vai a AIPMS Lab</a>`
+    labLink: `<a href="lab.html">Vai a AIPMS™ Lab</a>`
   },
   en: {
     history: "Founded in 2016, AIPMS was created to unite innovation and progress, promoting a future vision where Artificial Intelligence operates as humanity’s ally, serving development, knowledge, and collective progress.",
     message: "Our website is under construction. Check back soon!",
-    labLink: `<a href="lab.html">Go to AIPMS Lab</a>`
+    labLink: `<a href="lab.html">Go to AIPMS™ Lab</a>`
   },
   de: {
     history: "Gegründet 2016, wurde AIPMS geschaffen, um Innovation und Fortschritt zu vereinen und eine Zukunftsvision zu fördern, in der Künstliche Intelligenz als Verbündete der Menschheit dient.",
     message: "Unsere Website befindet sich im Aufbau. Schauen Sie bald wieder vorbei!",
-    labLink: `<a href="lab.html">Zum AIPMS Lab</a>`
+    labLink: `<a href="lab.html">Zum AIPMS™ Lab</a>`
   }
 };
 
